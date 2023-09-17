e48e1cb40ab54ec6288e1d7a7d0ee36f900b0262
Juan Carlos Moreno Sanchez <carlos.moreno.phd@gmail.com>
2023-09-17 11:47:06 -0500

    Adding .gitignore and upgrade database

--- api-phenotypic/database.py
+++ api-phenotypic/database.py
@@ -1,15 +1,15 @@
 import os
-import logging
-import peewee
+form peewee import _ConnectionState, MySQLDatabase
 from dotenv import load_dotenv
 from contextvars import ContextVar
 
+load_dotenv()
 DATABASE_NAME = os.getenv('DB_SCHEMA')
 db_state_default = {"closed": None, "conn": None, "ctx": None, "transactions": None}
 db_state = ContextVar("db_state", default=db_state_default.copy())
 
 
-class PeeweeConnectionState(peewee._ConnectionState):
+class PeeweeConnectionState(_ConnectionState):
     def __init__(self, **kwargs):
         super().__setattr__("_state", db_state)
         super().__init__(**kwargs)
@@ -21,6 +21,6 @@ class PeeweeConnectionState(peewee._ConnectionState):
         return self._state.get()[name]
 
 
-db = peewee.MySQLDatabase(DATABASE_NAME, host=os.getenv("DB_HOST"), port=os.getenv('DB_PORT'), user=os.getenv('DB_USER'), password=os.getenv('DB_PASS'))
+db = MySQLDatabase(DATABASE_NAME, host=os.getenv("DB_HOST"), port=os.getenv('DB_PORT'), user=os.getenv('DB_USER'), password=os.getenv('DB_PASS'))
 
 db._state = PeeweeConnectionState()
