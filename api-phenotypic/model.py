f6a7cb2afee1a4546cf1554dc49696275fe8231a
Juan Carlos Moreno Sanchez <carlos.moreno.phd@gmail.com>
2023-09-17 10:29:49 -0500

    Create new Model

--- api-phenotypic/model.py
+++ api-phenotypic/model.py
@@ -1,144 +1,145 @@
-import peewee
-
+from peewee import Model, CharField, ForeignKeyField, IntegerField, FixedCharField, TextField
+# import peewee
 from database import db
 
 
-class WebFile(peewee.Model):
-    name = peewee.CharField()
-
-    class Meta:
-        database = db
-
-
-# class Item(peewee.Model):
-#     title = peewee.CharField(index=True)
-#     description = peewee.CharField(index=True)
+# class Item(Model):
+#     title = CharField(index=True)
+#     description = CharField(index=True)
 #     owner = peewee.ForeignKeyField(User, backref="items")
 
 #     class Meta:
 #         database = db
 
-class Trail(peewee.Model):
-    name = peewee.CharField(unique=True, index=True)
+class WebFile(Model):
+    name = CharField()
 
     class Meta:
         database = db
 
-class Unit(peewee.Model):
-    name = Column(String(300))
+class Trail(Model):
+    name = CharField(unique=True, index=True)
+
+    class Meta:
+        database = db
+
+class Unit(Model):
+    name = CharField(unique=True, index=True)
 
     class Meta:
         database = db
 
 
-class Trait(peewee.Model):
+class Trait(Model):
     __tablename__ = 'Trait'
-    id = Column(Integer, primary_key=True),
-    name = Column(String(300)),
-    number = Column(Integer),
-    description = Column(String(500)),
-    co_trait_name = Column(String(200)),
-    variable_name = Column(String(200)),
-    co_id = Column(String(400)),
-    variable_ontology_id = Column(Integer, ForeignKey("VariableOntology.id"), nullable=True),
-    variable_ontology = relationship("VariableOntology", back_populates="traits")
+    name = CharField(unique=True, index=True)
+    number = CharField()
+    description = FixedCharField()
+    co_trait_name = CharField(index=True)
+    variable_name = CharField()
+    co_id = CharField()
+    # variable_ontology = ForeignKeyField(VariableOntology, backref="traits")
+
+    class Meta:
+        database = db
 
 
-class Genotype(peewee.Model):
-    __tablename__ = 'Genotype'
-    id = Column(Integer, primary_key=True),
-    c_id = Column(Integer),
-    s_id = Column(Integer),
-    cross_name = Column(String(600)),
-    history = Column(String(600)),
+class Genotype(Model):
+    c_id = IntegerField()
+    s_id = IntegerField()
+    cross_name = FixedCharField()
+    history = FixedCharField()
 
 
-class Location(peewee.Model):
-    __tablename__ = 'Location'
-    id = Column(Integer, primary_key=True)
-    number = Column(Integer)
-    country = Column(String(600))
-    description = Column(String(600))
-    institute_name = Column(String(600))
-    cooperator = Column(String(600))
-    latitude = Column(String(10))
-    latitude_degrees = Column(Integer)
-    latitude_minutes = Column(Integer)
-    longitude = Column(String(10))
-    longitude_degrees = Column(Integer)
-    longitude_minutes = Column(Integer)
-    altitude = Column(Integer)
+class Location(Model):
+    number = IntegerField()
+    country = CharField()
+    description = FixedCharField()
+    institute_name = CharField()
+    cooperator = CharField()
+    latitude = CharField()
+    latitude_degrees = IntegerField()
+    latitude_minutes = IntegerField()
+    longitude = CharField()
+    longitude_degrees = IntegerField()
+    longitude_minutes = IntegerField()
+    altitude = IntegerField()
+
+    class Meta:
+        database = db
 
 
-class CropOntology(peewee.Model):
-    __tablename__ = 'CropOntology'
-    id = Column(Integer, primary_key=True)
-    ontologyDbId = Column(String(50))
-    name = Column(String(200))
-    trait_ontologies = relationship("TraitOntology", back_populates="crop_ontology")
+class CropOntology(Model):
+    ontologyDbId = CharField()
+    name = CharField()
+    # trait_ontologies = ForeignKeyField(TraitOntology, backref="crop_ontology")
+
+    class Meta:
+        database = db
 
 
-class TraitOntology(peewee.Model):
-    __tablename__ = 'TraitOntology'
-    id = Column(Integer, primary_key=True)
-    traitDbId = Column(String(50))
-    name = Column(String(200))
-    class_family = Column(String(200))
-    description = Column(String(1000))
-    crop_ontology_id = Column(Integer, ForeignKey("CropOntology.id"), nullable=False),
-    crop_ontology = relationship("CropOntology", back_populates="trait_ontologies")
-    variable_ontologies = relationship("VariableOntology", back_populates="trait_ontology")
+class TraitOntology(Model):
+    traitDbId = CharField()
+    name = CharField()
+    class_family = CharField()
+    description = TextField()
+    crop_ontology = ForeignKeyField(CropOntology, backref="trait_ontologies")
+    # variable_ontologies = ForeignKeyField(VariableOntology, backref="trait_ontology")
+
+    class Meta:
+        database = db
 
 
-class MethodOntology(peewee.Model):
-    __tablename__ = 'MethodOntology'
-    id = Column(Integer, primary_key=True)
-    methodDbId = Column(String(50))
-    name = Column('', String(200))
-    class_family = Column(String(200))
-    description = Column(String(1000))
-    formula = Column(String(500))
-    variable_ontologies = relationship("VariableOntology", back_populates="method_ontology")
+class MethodOntology(Model):
+    methodDbId = CharField()
+    name = CharField()
+    class_family = CharField()
+    description = TextField()
+    formula = FixedCharField()
+    # variable_ontologies = ForeignKeyField(VariableOntology, backref="method_ontology")
+
+    class Meta:
+        database = db
 
 
-class ScaleOntology(peewee.Model):
-    __tablename__ = 'ScaleOntology'
-    id = Column(Integer, primary_key=True)
-    scaleDbId = Column(String(50))
-    name = Column(String(200))
-    dataType = Column(String(200))
-    validValues = Column(String(2000))
-    variable_ontologies = relationship("VariableOntology", back_populates="scale_ontology")
+class ScaleOntology(Model):
+    scaleDbId = CharField(unique=True, index=True)
+    name = CharField()
+    dataType = CharField()
+    validValues = TextField()
+    # variable_ontologies = ForeignKeyField(VariableOntology, backref="scale_ontology")
+
+    class Meta:
+        database = db
 
 
-class VariableOntology(peewee.Model):
-    __tablename__ = 'VariableOntology'
-    id = Column(Integer, primary_key=True)
-    trait_ontology_id = Column(Integer, ForeignKey("TraitOntology.id"), nullable=False),
-    trait_ontology = relationship("TraitOntology", back_populates="variable_ontologies")
-    traits = relationship("Trait", back_populates="variable_ontology")
-    method_ontology_id = Column(Integer, ForeignKey("MethodOntology.id"), nullable=False)
-    method_ontology = relationship("MethodOntology", back_populates="variable_ontologies")
-    scale_ontologies_id = Column(Integer, ForeignKey("ScaleOntology.id"), nullable=False)
-    scale_ontology = relationship("ScaleOntology", back_populates="variable_ontologies")
-    observation_Variable_db_id = Column(String(50))
-    name = Column(String(200))
-    synonyms = Column(String(500))
-    growth_stage = Column(String(500))
+class VariableOntology(Model):
+    name = CharField()
+    synonyms = FixedCharField()
+    growth_stage = FixedCharField()
+    observation_variable_db_id = CharField()
+    trait_ontology = ForeignKeyField(TraitOntology, backref="variable_ontologies")
+    traits = ForeignKeyField(Trait, backref="variable_ontology")
+    method_ontology = ForeignKeyField(MethodOntology, backref="variable_ontologies")
+    scale_ontology = ForeignKeyField(ScaleOntology, backref="variable_ontologies")
+
+    class Meta:
+        database = db
 
 
-class RawCollection(peewee.Model):
-    __tablename__ = 'RawCollection'
-    id = Column(Integer, primary_key=True)
-    trail_id = Column(Integer, ForeignKey("Trail.id"), nullable=False),
-    trait_id = Column(Integer, ForeignKey("Trait.id"), nullable=False),
-    genotype_id = Column(Integer, ForeignKey("Genotype.id"), nullable=False),
-    location_id = Column(Integer, ForeignKey("Location.id"), nullable=False),
-    occurrence = Column(Integer),
-    cycle = Column(String(4)),
-    gen_number = Column(Integer),
-    repetition = Column(Integer),
-    sub_block = Column(Integer),
-    value_data = Column(String(100)),
+class RawCollection(Model):
+    occurrence = IntegerField()
+    cycle = CharField()
+    gen_number = IntegerField()
+    repetition = IntegerField()
+    sub_block = IntegerField()
+    value_data = CharField()
+    trail = ForeignKeyField(Trail, backref="raw_collection")
+    trait = ForeignKeyField(Trait, backref="raw_collection")
+    genotype = ForeignKeyField(Genotype, backref="raw_collection")
+    location = ForeignKeyField(Location, backref="raw_collection")
+    
+    class Meta:
+        database = db
 
 
