b2e5ee94834e1c33e8caa844b859feabee0029bc
Juan Carlos Moreno Sanchez <carlos.moreno.phd@gmail.com>
2023-09-29 07:15:57 -0600

    Adding new endpoint to get raw data

--- app/cruds/rawCrud.py
+++ app/cruds/rawCrud.py
@@ -3,6 +3,7 @@ from csv import writer
 import os
 
 # TODO: Convertir en dinamica para poder seleccionar con una lista de traits
+# TODO: soportar la unidad dentro de los traits
 query_raw_all = "SELECT raj.id, raj.occurrence, raj.`cycle`, raj.gen_number, raj.repetition, raj.sub_block, raj.plot, raj.genotype_id, raj.location_id, \
 g.cross_name, l.country , l.description , l.institute_name , \
 raj.value_1,'t/ha' as u_1,  raj.value_2, 'g' as u_2, raj.value_3,'cm' as u_3, raj.value_5, '1-5' as u_5, raj.value_6, 'dias' as u_6, raj.value_12, 'dias' as u_12, raj.value_14, 'doble digito' as u_14 \
@@ -10,7 +11,8 @@ FROM raw_all_join raj \
 LEFT JOIN genotype g ON g.id =raj.genotype_id \
 LEFT JOIN location l ON l.id = raj.location_id "
 
-# TODO: soportar la unidad dentro de los traits
+
+
 def get_raw_join_all(raw_filter: schemas.RawAllFilter) -> str:
     name_csv = "text.csv"
     if os.path.exists(name_csv):
