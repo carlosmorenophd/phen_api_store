fb9e7aac30e3bc6f46b69d9fe3f7eae0ba6a93de
Juan Carlos Moreno Sanchez <carlos.moreno.phd@gmail.com>
2024-06-11 03:17:54 +0000

    Enable new 3 endpoints to search

--- README.md
+++ README.md
@@ -28,3 +28,32 @@ Install requiremntes
 ```py
 pip install -r requirements.txt
 ```
+
+# To run don docker
+
+## Run on production
+Command to build image
+
+```
+docker build --tag phen/api_store:24.06 .
+```
+
+To run the container
+
+```
+docker run --name api_store -p 8081:8000 -d --network=net-phenotypic -e DATABASE_USERNAME=admin -e DATABASE_PASSWORD=Fantasy24 -e DATABASE=phenotypic_db -e DATABASE_HOST=mariadb_phenotypic -e DATABASE_SOCKET=3306 -e FTP_HOSTNAME=ftp_phenotypic -e FTP_PORT=21 -e FTP_USERNAME=user -e FTP_PASSWORD=ftp1221Wheat phen/api_store:24.05
+```
+
+## Run on dev
+
+Create image to dev project
+
+```
+docker build --tag phen/api_store:00.dev -f Dockerfile.dev .
+```
+
+Run image in a container
+
+```
+docker run -it -d --name dev_api_store -p 8091:8000 --network=net-phenotypic -v ${PWD}:/develop  phen/api_store:00.dev
+```%
--- app/routes/genotypeRoute.py
+++ app/routes/genotypeRoute.py
@@ -2,17 +2,18 @@ from fastapi import APIRouter, Depends, HTTPException
 from schemas import schemas
 from cruds import genotypeCrud
 from dependencies import get_db
-
+from typing import Union
 
 router = APIRouter(
     prefix="/genotypes",
     tags=["Genotype"],
-    responses={404: {"description": "Genotype not found"}}
+    responses={404: {"description": "Genotype not found"}},
 )
 
 
 @router.post(
-    "/", response_model=schemas.Genotype,
+    "/",
+    response_model=schemas.Genotype,
     dependencies=[Depends(get_db)],
     tags=["Genotype"],
 )
@@ -21,7 +22,7 @@ def create_genotype(genotype: schemas.GenotypeCreate):
 
 
 @router.get(
-    "/",
+    "/ids",
     response_model=schemas.Genotype,
     dependencies=[Depends(get_db)],
 )
@@ -29,8 +30,31 @@ def find_genotype_by_ids(c_id: int, s_id: int):
     try:
         return genotypeCrud.find_by_ids(c_id=c_id, s_id=s_id)
     except ValueError as err:
-        raise HTTPException(
-            status_code=404, detail="Genotype not found") from err
+        raise HTTPException(status_code=404, detail="Genotype not found") from err
+    
+    
+@router.get(
+    "/c_id",
+    response_model=schemas.Genotype,
+    dependencies=[Depends(get_db)],
+)
+def find_genotype_by_c_id(c_id: int):
+    try:
+        return genotypeCrud.find_by_ids(c_id=c_id, s_id=None)
+    except ValueError as err:
+        raise HTTPException(status_code=404, detail="Genotype not found") from err
+
+
+@router.get(
+    "/s_id",
+    response_model=schemas.Genotype,
+    dependencies=[Depends(get_db)],
+)
+def find_genotype_by_s_id(s_id: int):
+    try:
+        return genotypeCrud.find_by_ids(c_id=None, s_id=s_id)
+    except ValueError as err:
+        raise HTTPException(status_code=404, detail="Genotype not found") from err
 
 
 @router.get(
@@ -43,5 +67,4 @@ def find_genotype_by_id(id: int):
     try:
         return genotypeCrud.find_by_id(id=id)
     except ValueError as err:
-        raise HTTPException(
-            status_code=404, detail="Genotype not found") from err
+        raise HTTPException(status_code=404, detail="Genotype not found") from err
